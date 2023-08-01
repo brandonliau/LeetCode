3f322dbb949a36b122a2e39e2839979568c8f807
brandonliau <bliauu03@gmail.com>
2023-08-01 00:13:04 -0400

    completed: 1 medium

--- NeetCode 150/1. Arrays and Hashing/LongestConsecutiveSequence.py
+++ NeetCode 150/1. Arrays and Hashing/LongestConsecutiveSequence.py
@@ -1,3 +1,11 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        
+        numset, maxlength = set(nums), 0
+
+        for n in nums:
+            if n - 1 not in numset:
+                length = 1
+                while n + length in numset:
+                    length += 1
+                maxlength = max(length, maxlength)
+        return maxlength
