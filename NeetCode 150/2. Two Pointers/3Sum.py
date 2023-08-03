ccf0df0a38763f22d026f3478b14703b8b45c34e
brandonliau <bliauu03@gmail.com>
2023-08-03 00:07:45 -0400

    completed: 1 easy, 2 medium

---
+++ NeetCode 150/2. Two Pointers/3Sum.py
@@ -0,0 +1,25 @@
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        ans, length = [], len(nums)
+        nums.sort()
+        for i in range(length):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            point, left, right = i, i + 1, length - 1
+            while left < right:
+                sum = nums[point] + nums[left] + nums[right]
+                if sum < 0:
+                    left += 1
+                elif sum > 0:
+                    right -= 1
+                else:
+                    ans.append([nums[point], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    while nums[left] == nums[left - 1] and left < right:
+                        left += 1
+        return ans
+
+nums = [-1,0,1,2,-1,-4]
+sol = Solution()
+print(sol.threeSum(nums))
--- NeetCode 150/2. Two Pointers/validPalindrome.py
+++ NeetCode 150/2. Two Pointers/validPalindrome.py
@@ -1,9 +1,13 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        string = ''
-        for char in s:
-            if char.isalnum:
-                string += char.lower()
-
-        return string == string[::-1]
-
+        left, right = 0, len(s) - 1
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+        return True
